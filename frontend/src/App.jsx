c12b60cf331e539b6647700e40480dcbcbc07c13
tai-shis <tshis070@mtroyal.ca>
2025-09-18 15:18:37 -0600

    Added frontend post form request

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
+import axios from 'axios';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const fetchAPI = async () => {
+  //   const response = await axios.get('http://localhost:8080/classes');
+  //   setArray(response.data.classes);
+  //   setYear(response.data.year);
+  //   setGPA(response.data.GPA);
+  //   console.log(response.data);
+  // };
+
+  // const testPost = async () => {
+  //     const response = await axios.post('http://localhost:8080/signup', { email: 'foo@bar.com', name: 'foo' })
+  //     console.log(response.data);
+  // };
+
+  const [formData, setFormData] = useState({
+    userName: '',
+    email: '',
+    plaintext: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+  try {
+    const response = await axios.post('http://localhost:8080/signup', formData);
+    console.log('Form data submitted successfully:', response.data);
+  } catch (error) {
+    console.error('Error submitting form data:', error);
+  }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <form onSubmit={handleSubmit}>
+      <label>Username:</label>
+      <br/>
+      <input 
+        type="text"
+        name="userName"
+        placeholder="Enter Username"
+        value={formData.userName}
+        onChange={handleChange}
+      />
+      <br/>
+
+      <label>Email:</label>
+      <br/>
+      <input 
+        type="email"
+        name="email"
+        placeholder="foo@bar.com"
+        value={formData.email}
+        onChange={handleChange}
+      />
+      <br/>
+      <label>Password:</label>
+      <br/>
+      <input 
+        type="password"
+        name="plaintext"
+        placeholder="Enter Password"
+        value={formData.plaintext}
+        onChange={handleChange}
+      />
+      <br/>
+      <input type="submit"/>
+    </form> 
   )
 }
 
